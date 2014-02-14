44c91ad6567b04a153413272d56e6ce7ab8abb76
Grant McKenzie <grantdmckenzie@gmail.com>
2014-02-13 18:57:22 -0800

    trying to get leaflet to supress not found tiles

--- js/layers.js
+++ js/layers.js
@@ -1,48 +1,19 @@
     
-    
-    
-    var layers = {};
-    var _base = {};
+  
+    // CONFIG
     var _groupNum = 1;
-    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     var _sig = 1;
-    var _geoserver_base = "http://stko-poi.geog.ucsb.edu:8080/geoserver/gwc/service/wms/";
-    var _namespace = "stko:lvg_";   
+ 
+    var _map = L.map('map').setView([33.968064, -118.171692], 10);
+    L.tileLayer('http://{s}.tile.cloudmade.com/d1abd7c11778439c95f45e03924ab211/115014/256/{z}/{x}/{y}.png', {
+	maxZoom: 18,
+    }).addTo(_map);
+    
+    _LAPULSE.constructBase();
+    _LAPULSE.drawLegend();
+    _LAPULSE.showLayer(1);
     
 
-    _base.residence = {color:'f781bf',layer:{},name:'Residence', id: '4e67e38e036454776db1fb3a'};
-    _base.college = {color:'377eb8',layer:{}, name: 'College & University', id: '4d4b7105d754a06372d81259'};
-    _base.arts = {color:'e41a1c',layer:{},name:'Arts & Entertainment', id: '4d4b7104d754a06370d81259'};
-    _base.nightlife = {color:'ff7f00',layer:{},name:'Nightlife Spot', id: '4d4b7105d754a06376d81259'};
-    _base.food = {color:'999',layer:{},name:'Food', id: '4d4b7105d754a06374d81259'};
-    // _base.event = {color:'ff3',layer:{}};
-    _base.outdoor = {color:'ff3',layer:{},name:'Outdoors & Recreation', id: '4d4b7105d754a06377d81259'};
-    _base.professional = {color:'a65628',layer:{},name:'Professional & Other Places', id: '4d4b7105d754a06375d81259'};
-    _base.shop = {color:'984ea3',layer:{},name:'Shop & Service', id: '4d4b7105d754a06378d81259'};
-    _base.travel = {color:'4daf4a',layer:{},name:'Travel & Transport', id: '4d4b7105d754a06379d81259'};
-
-    //getLayers();
-    //drawLegend(_base);
-    //showLayer(1);
-    
-    function getLayers() {
-	for(var i=_groupNum; i<	(_groupNum+1);i++) {
-	    for(var key in _base) {
-		/*layers[key+'_'+i] = new L.TileLayer.WMS(_geoserver_base+"stko/wms", {
-		    layers: ''+_namespace+key,
-		    format: 'image/png',
-		    transparent: true,
-		    styles: "la_"+key.substring(0,4)+"_"+i,
-		    // sld: "http://stko-poi.geog.ucsb.edu/la/sld.php/"+layers[key].color+"/1/"+key,
-		    version: '1.1.1'
-		}); */
-		layers[key+'_'+i] = new L.TileLayer.GWC('http://stko-poi.geog.ucsb.edu/la/gwc/stko_lvg_'+key+'/l{z}_'+i+'/{dir_x}_{dir_y}/{x}_{y}.png',{tms:true});
-		layers[key+'_'+i].addTo(_map);
-		layers[key+'_'+i].setOpacity(0);
-	    }
-	}
-	_groupNum+=1;
-    }
     
     
     function toggleLayer(name) {
@@ -53,9 +24,29 @@
 	  document.getElementById(b[0]+'_c').style.backgroundColor = '#eeeeee';
 	} else {
 	  layers[name].setOpacity(1);
-	  document.getElementById(b[0]+'_c').style.backgroundColor = '#'+_base[b[0]].color;
+	  document.getElementById(b[0]+'_c').style.backgroundColor = '#'+this.base[b[0]].color;
 	}
 	
+    }
+    
+
+
+    function getNextTime() {
+	// Update Time Label
+	var num = _sig % 24;
+	var day = days[Math.floor(_sig/24)];
+	document.getElementById('lbl').innerHTML = day + " " + num + ":00";
+	
+	getLayers();
+	_sig++;
+	showLayer(_sig);
+	/*if (_sig%3==2) {
+	   getLayers();
+	} */
+    }
+      
+    function drawLegend(base) {
+
     }
   
     
--- scripts/geowebcache/seedLayers.php
+++ scripts/geowebcache/seedLayers.php
@@ -9,9 +9,11 @@
       foreach($a as $aa) {
 	$s = explode("_",$aa);
 	$style = "la_".substr($s[1],0,4)."_".$i;
-        $curl = 'curl -v -u '.$geoserver_username.':'.$geoserver_password.' -XPOST -H "Content-type: text/xml" -d "<seedRequest><name>stko:'.$aa.'</name><srs><number>900913</number></srs><zoomStart>10</zoomStart><zoomStop>10</zoomStop><format>image/png</format><type>reseed</type><threadCount>1</threadCount><parameters><entry><string>STYLES</string><string>'.$style.'</string></entry></parameters></seedRequest>"  "'.$geoserver_url.'/geoserver/gwc/rest/seed/stko:'.$aa.'.xml"';
-	exec($curl);
-	sleep(60);
+        $curl = 'curl -v -u '.$geoserver_username.':'.$geoserver_password.' -XPOST -H "Content-type: text/xml" -d "<seedRequest><name>stko:'.$aa.'</name><gridSetId>EPSG:900913</gridSetId><bounds><coords><double>-14229032.07304346</double><double>1973162.274517791</double><double>-6081762.549246961</double><double>6072936.4705572966</double></coords></bounds><zoomStart>10</zoomStart><zoomStop>10</zoomStop><format>image/png</format><type>reseed</type><threadCount>1</threadCount><parameters><entry><string>STYLES</string><string>'.$style.'</string></entry></parameters></seedRequest>"  "'.$geoserver_url.'/geoserver/gwc/rest/seed/stko:'.$aa.'.xml"';
+	//exec($curl);
+	//sleep(60);
+	echo $curl . "\n";
       }
   }
 ?>
+
