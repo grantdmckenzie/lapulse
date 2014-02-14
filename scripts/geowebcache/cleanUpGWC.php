f92646ffed6142e41fd966969ee12a0146ca1c0f
Grant McKenzie <grantdmckenzie@gmail.com>
2014-02-13 17:52:46 -0800

    minor changes to GWC workflow

--- scripts/geowebcache/cleanUpGWC.php
+++ scripts/geowebcache/cleanUpGWC.php
@@ -2,14 +2,14 @@
   /*  @author: Grant McKenzie
       @date: 30/01/2014
       @contact: grantmckenzie.com
-      @function: rename the mest up directory structure that GeoWebCache inflicts on us.
+      @function: rename the messed up directory structure that GeoWebCache inflicts on us.
   */
   
   $base = "/var/www/la/";
-  if ($handle = opendir('gwc/')) {
+  if ($handle = opendir($base.'gwc/')) {
       while (false !== ($entry = readdir($handle))) {
 	  if (substr($entry,0,4) == "stko") {
-	      if ($handle2 = opendir("gwc/".$entry)) {
+	      if ($handle2 = opendir($base."gwc/".$entry)) {
 		  $files = array();
 		  while (false !== ($file = readdir($handle2))) {
 		      if ($file != "." && $file != "..") {
@@ -25,8 +25,10 @@
 		      $newname = $base."gwc/".$entry."/l10_".$count;
 		      rename($oldname, $newname);
 		      $oldname = $base."gwc/".$entry."/EPSG_900913_11_".$d[3];
-		      $newname = $base."gwc/".$entry."/l11_".$count;
-		      rename($oldname, $newname);
+		      if(file_exists($oldname)) {
+			$newname = $base."gwc/".$entry."/l11_".$count;
+			rename($oldname, $newname);
+		      }
 		      $count++;
 		    }
 		  }
--- scripts/geowebcache/deleteGWC.php
+++ scripts/geowebcache/deleteGWC.php
@@ -2,14 +2,14 @@
   /*  @author: Grant McKenzie
       @date: 30/01/2014
       @contact: grantmckenzie.com
-      @function: delete the mest up directory structure that GeoWebCache inflicts on us.
+      @function: delete the messed up directory structure that GeoWebCache inflicts on us.
   */
   
   $base = "/var/www/la/";
-  if ($handle = opendir('gwc/')) {
+  if ($handle = opendir($base.'gwc/')) {
       while (false !== ($entry = readdir($handle))) {
 	  if (substr($entry,0,4) == "stko") {
-	      if ($handle2 = opendir("gwc/".$entry)) {
+	      if ($handle2 = opendir($base."gwc/".$entry)) {
 		  $files = array();
 		  while (false !== ($file = readdir($handle2))) {
 		      if ($file != "." && $file != "..") {
